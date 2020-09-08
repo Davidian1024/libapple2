38f83fc3f8e2175e0bbdf11e9b75eb762426b969
David Brinovec <david.brinovec@gmail.com>
2020-09-08 14:21:42 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Davidian1024/libapple2
+
+go 1.12
---
+++ libapple2.go
@@ -0,0 +1,14 @@
+package libapple2
+
+import "fmt"
+/*
+#include <stdlib.h>
+#include "libretro.h"
+*/
+import (
+	"C"
+)
+
+func main() {
+	fmt.Println("libapple2")
+}
