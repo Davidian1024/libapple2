fd65a12c904857b533b4a672e93c9a0e2ad4181c
David Brinovec <david.brinovec@gmail.com>
2020-09-16 22:23:20 -0400

    Got RetroArch to call retro_get_system_info()

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # libapple2
 A libretro core for Apple ][ emulation
+
+
+# foundational resources
+github.com/libretro/RetroArch
+github.com/ivanizag/apple2
+
+# building
+# optimizations disabled
+go build -gcflags '-N -l' -o libgithub.com-Davidian1024-libapple2.so -buildmode=c-shared .
+go install -gcflags '-N -l' -buildmode=shared .
+
--- libapple2.go
+++ libapple2.go
@@ -1,6 +1,9 @@
-package libapple2
+//package libapple2
+package main
 
 import "fmt"
+//import "unsafe"
+
 /*
 #include <stdlib.h>
 #include "libretro.h"
@@ -9,6 +12,31 @@ import (
 	"C"
 )
 
-func main() {
-	fmt.Println("libapple2")
+//export retro_get_system_info
+func retro_get_system_info(info *C.struct_retro_system_info) {
+	fmt.Println("libapple2 retro_get_system_info() start")
+	info.need_fullpath    = false;
+	info.valid_extensions = C.CString("dsk")
+	info.library_version = C.CString("0.1")
+	info.library_name = C.CString("libapple2")
+	info.block_extract    = false;
+
+	fmt.Println(info.need_fullpath)
+	fmt.Println(*info.valid_extensions)
+	fmt.Println(*info.library_version)
+	fmt.Println(*info.library_name)
+	fmt.Println(info.block_extract)
+	fmt.Println("libapple2 retro_get_system_info() end")
+	return
 }
+
+//func main() {
+//	fmt.Println("libapple2")
+//}
+
+//export retro_init
+func retro_init() {
+	fmt.Println("libapple2 retro_init()")
+}
+
+func main() {}
